95b48d5ec9ad0f93545c6b6ee4fac798e5dab753
edison <ingedisonperdomo@gmail.com>
2022-06-16 16:31:26 -0400

    seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
-
+export class Tab1Page implements OnInit {
   constructor() {}
 
+  ngOnInit() {
+    const numero = 10;
+    const numero2 = 20;
+    //debugger;
+    const suma = numero + numero2;
+    console.log(suma);
+  }
 }
